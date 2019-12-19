9c752fc64f0ecb38589d4b1062c4684a67f28bf4
candy12t <candy12t@gmail.com>
2019-12-20 00:41:41 +0900

    2値化

--- 12_19/code/task2.m
+++ 12_19/code/task2.m
@@ -13,7 +13,24 @@ H = [-1,-1,-1; -1,8,-1; -1,-1,-1;];
 
 YF = mask(YO, delta, C, R, H);
 
+N = 128;
+th = 120; % âÊëúÇÃîZíWÇçïÇ∆îíÇÃ2ílâªÇ∑ÇÈÇΩÇﬂÇÃèâä˙âª
+BY = zeros(N, N); % 2ílâªâÊëúÇäiî[Ç∑ÇÈîzóÒÇÃèâä˙âª
+for i=1:N
+    for j=1:N
+        if YF(i, j) >= th
+            BY(i, j) = 1; % 2ílâª(îí)
+        else
+            BY(i, j) = 0; % 2ílâª(çï)
+        end
+    end
+end
+
 figure(1)
 imshow(uint8(YO)); xlabel('Original image');
 figure(2)
 imshow(uint8(YF)); xlabel('Filtered image');
+figure(3)
+imshow(BY); xlabel('Binary image');
+
+
