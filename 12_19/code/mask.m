546110a94882d7e7cdda1c7b0c8232d95c4298f2
candy12t <candy12t@gmail.com>
2019-12-21 22:18:18 +0900

    コメント追加

--- 12_19/code/mask.m
+++ 12_19/code/mask.m
@@ -1,10 +1,10 @@
 function [YF] = mask(YO, delta, C, R, H)
-YF = YO;
-for i = 1+delta:C-delta
-    for j = 1+delta:R-delta
-        S = double(YO(i-delta:i+delta,j-delta:j+delta));
-        YF(i, j) = sum(sum(H.*S));
+    YF = YO; % フィルタ処理用の画像を格納する配列を元画像地に初期化
+    for i = 1+delta:C-delta % (i,j)はマスク中心位置(iは縦方向ループ)
+        for j = 1+delta:R-delta % (i,j)はマスク中心位置(jは横方向ループ)
+            S = double(YO(i-delta:i+delta,j-delta:j+delta)); % フィルタ処理する画像範囲
+            YF(i, j) = sum(sum(H.*S)); % フィルタ係数を乗じた総和を出力
+        end
     end
-end
 end
 
--- 12_19/code/task2.m
+++ 12_19/code/task2.m
@@ -1,17 +1,17 @@
 close all; clear; clc
 
-X0 = imread('../../sample_image/Parrots.jpg');
-X = imresize(X0, [128, 128]);
-YX = rgb2ycbcr(X);
-YO = YX(:,:,1);
-[C, R] = size(YO);
+X0 = imread('../../sample_image/Parrots.jpg'); % 画像読み込み
+X = imresize(X0, [128, 128]); % 画像サイズの設定
+YX = rgb2ycbcr(X); % RGB色空間からYCbCr色空間に変換
+YO = YX(:,:,1); % グレースケール画像(Y成分画像)
+[C, R] = size(YO); % 画像の縦横サイズ取得(Cは縦、Rは横)
 
-K = 3;
-delta = fix(K/2);
+K = 3; % マスクサイズ(フィルタサイズ)
+delta = fix(K/2); % マスク中心位置調整用の変数(その要素以下の最も近い整数)
 
-H = [-1,-1,-1; -1,8,-1; -1,-1,-1;];
+H = [-1,-1,-1; -1,8,-1; -1,-1,-1;]; % エッジ抽出フィルタ係数(3×3行列)
 
-YF = mask(YO, delta, C, R, H);
+YF = mask(YO, delta, C, R, H); % 局所マスク関数
 
 N = 128;
 th = 120; % 画像の濃淡を黒と白の2値化するための初期化
@@ -27,9 +27,9 @@ for i=1:N
 end
 
 figure(1)
-imshow(uint8(YO)); xlabel('Original image');
+imshow(uint8(YO)); xlabel('Original image'); % 元画像表示
 figure(2)
-imshow(uint8(YF)); xlabel('Filtered image');
+imshow(uint8(YF)); xlabel('Filtered image'); % フィルタ処理画像表示
 figure(3)
 imshow(BY); xlabel('Binary image');
 
